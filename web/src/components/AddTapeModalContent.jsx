977b0203dda68775bf3a767a0e25d2342b2ce658
Airrick Dunfield <hello@airrickdunfield.com>
2025-02-13 09:22:44 -0800

    Improves modal styles

--- web/src/components/AddTapeModalContent.jsx
+++ web/src/components/AddTapeModalContent.jsx
@@ -1,9 +1,10 @@
 import m from "./AddTapeModalContent.module.css";
+import g from "../global.module.css";
 
 export default function ModalContent({ onClose }) {
     return (
       <div className={m['modal-container']}>
-      <div className={m['modal']}>
+      <div className={`${m['modal']} ${g['card']}`}>
         <div>Im a modal dialog</div>
         <button onClick={onClose} className={m["modal__close-button"]}>x</button>
       </div>

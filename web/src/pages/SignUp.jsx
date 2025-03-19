df43d8a994d175ad5658168424631e2b6de43dc7
Airrick Dunfield <hello@airrickdunfield.com>
2025-03-19 10:15:05 -0700

    Adds forms for sign up and sign in

--- web/src/pages/SignUp.jsx
+++ web/src/pages/SignUp.jsx
@@ -1,8 +1,40 @@
+import React, { useState } from 'react';
 import g from '../global.module.css';
 
 import bannerImage from '../assets/images/home-bg.jpg';
 
 function SignUp() {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });    
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (formData.password !== formData.confirmPassword) {
+            alert('Passwords do not match!');
+            return;
+        }
+
+        fetch('https://your-api-domain.com/api/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: formData.email,
+                    password: formData.password
+                })
+            })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+            });
+
+    };    
 
     return (
         <main style={{backgroundImage: `url(${bannerImage})`}} className={`${g['container']} ${g["full-width"]} ${g['banner']}`}>
@@ -13,15 +45,32 @@ function SignUp() {
                         <form className={`${g['form-group']} ${g["form--full"]}`}>
                             <div >
                                 <label htmlFor="email">Email</label>
-                                <input type="email" id="email" name="email" />
+                                <input 
+                                    type="email" 
+                                    id="email" 
+                                    name="email"
+                                    placeholder='Email'
+                                    onChange={(e) => setFormData({...formData, email: e.target.value})} 
+                                />
                             </div>
                             <div >
                                 <label htmlFor="password">Password</label>
-                                <input type="password" id="password" name="password" />
+                                <input 
+                                    type="password" 
+                                    id="password" 
+                                    placeholder='Password'
+                                    name="password" 
+                                    onChange={(e) => setFormData({...formData, password: e.target.value})}
+                                />
                             </div>
                             <div >
                                 <label htmlFor="confirm-password">Confirm Password</label>
-                                <input type="password" id="confirm-password" name="confirm-password" />
+                                <input 
+                                    type="password" id="confirm-password" 
+                                    placeholder='Retype Password'
+                                    name="confirm-password" 
+                                    onChange={(e) => setFormData({...formData, confirmPassword: e.target.value})}
+                                />
                             </div>
                             <input type="submit" value="Register" className={`${g["button"]} ${g["success"]} `} />
 
